fcdc708d76d32600101a70bdd3677fa4f5df6901
Lucas Wihren <lucaswihren0402@gmail.com>
2026-09-23 14:45:58 +0200

    compleats the serialization lab, but i quite dont understand it

--- serialization.cpp
+++ serialization.cpp
@@ -1,56 +1,70 @@
+#include <iostream>
 #include <istream>
-#include <iostream>
+#include <limits>
 #include <sstream>
-#include <iostream>
-
 using namespace std;
 
-struct Product
-{
-    string name;
-    double price;
-    int inventory;
+struct Product {
+  string name;
+  double price;
+  int inventory;
 };
 
-ostream& operator<<(ostream& os, Product const& product)
-{
-    return os << product.name << ": "
-              << product.price << " kr ("
-              << product.inventory << " available)";
+ostream &operator<<(ostream &os, Product const &product) {
+  return os << product.name << ": " << product.price << " kr ("
+            << product.inventory << " available)";
 }
-H
-template <typename T>
-T read(istream& is
-{
-    T data;
-    is >> data;
-    return data;
+
+template <typename T> T read(istream &is) {
+  T data;
+  is >> data;
+  return data;
+}
+
+template <> string read<string>(istream &is) {
+  string str;
+  is.ignore(numeric_limits<streamsize>::max(), '#');
+  getline(is, str, '#');
+  return str;
+}
+
+template <> Product read<Product>(istream &is) {
+  return {read<string>(is), read<double>(is), read<int>(is)};
 }
 
 template <typename T>
-void write(ostream& os, T const& t)
+void write(ostream &os, T const &t) { os << t << ' '; }
+
+template<>
+void write<string>(ostream &os, string const &str)
 {
-    os << t << ' ';
+  os << "#" << str << "#";
 }
 
-int main()
-{
-    stringstream ss;
-    { // send data to ss
-        Product apple {"Red Apple", 1.5, 100};
-        Product laptop{"Laptop", 5995.0, 10};
-        Product monitor{"4K Monitor", 8790.0, 1};
-        write(ss, apple);
-        write(ss, laptop);
-        write(ss, monitor);
-    }
+template<>
+void write<Product>(ostream &os, Product const &obj) {
+  write(os, obj.name);
+  write(os, obj.price);
+  write(os, obj.inventory);
 
-    { // recieve data from ss
-        Product apple  {read<Product>(ss)};
-        Product laptop {read<Product>(ss)};
-        Product monitor{read<Product>(ss)};
-        cout << apple << endl
-             << laptop << endl
-             << monitor << endl;
-    }
+}
+
+
+int main() {
+  stringstream ss;
+  { // send data to ss
+    Product apple{"Red Apple", 1.5, 100};
+    Product laptop{"Laptop", 5995.0, 10};
+    Product monitor{"4K Monitor", 8790.0, 1};
+    write(ss, apple);
+    write(ss, laptop);
+    write(ss, monitor);
+  }
+
+  { // recieve data from ss
+    Product apple{read<Product>(ss)};
+    Product laptop{read<Product>(ss)};
+    Product monitor{read<Product>(ss)};
+    cout << apple << endl << laptop << endl << monitor << endl;
+  }
 }
